f410eee6cd5756ae3086b39a4a0c04d625d63205
yuuoota <93074354+yuuoota@users.noreply.github.com>
2021-12-30 02:30:59 +0900

    Userモデルのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+      validates :family_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' } do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_12_28_155420) do
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "birthday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
