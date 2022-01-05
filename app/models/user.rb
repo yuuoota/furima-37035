bbddfb9be0edbc9dbe87b3149d49b4b99fd14c22
yuuoota <93074354+yuuoota@users.noreply.github.com>
2022-01-05 21:48:46 +0900

    Userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,17 @@ class User < ApplicationRecord
     validates :nickname
     validates :birthday
 
-    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
       validates :family_name
       validates :first_name
     end
 
-    with_options format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' } do
+    with_options format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' } do
       validates :family_name_kana
       validates :first_name_kana
     end
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password = Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)
+    password {password}
+    password_confirmation {password}
+    family_name {Gimei.last.kanji}
+    first_name {Gimei.first.kanji}
+    family_name_kana {Gimei.last.katakana}
+    first_name_kana {Gimei.first.katakana}
+    birthday {Faker::Date.between(from: '1930-01-01', to: '2017-12-31')}
+  end
+end
