5016f6ec97a61173abc2738e27206ac9bba56189
yuuoota <93074354+yuuoota@users.noreply.github.com>
2022-01-17 14:45:34 +0900

    決済機能のサーバーサイドの実装

--- app/controllers/purchased_items_controller.rb
+++ app/controllers/purchased_items_controller.rb
@@ -5,11 +5,28 @@ class PurchasedItemsController < ApplicationController
   end
 
   def create
-    PurchasedItem.create(purchased_item_params)
+    @item = Item.find(params[:item_id])
+    @purchased_item = PurchasedItem.new(purchased_item_params)
+    if @purchased_item.valid?
+      pay_item
+      @purchased_item.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
   def purchased_item_params
-    params.permit(:item_id).merge(user_id: current_user.id)
+    params.permit(:item_id, :token).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchased_item_params[:token],
+        currency: 'jpy'
+      )
   end
 end
--- app/models/purchased_item.rb
+++ app/models/purchased_item.rb
@@ -1,4 +1,6 @@
 class PurchasedItem < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  attr_accessor :token
 end
